c381d46b87bf73549dba14c9dc8c9210c38c0504
ted555 <thanong984@gmail.com>
2018-08-07 17:06:34 +0700

    Finish function browsing photo and show that photo, add menu to register fragment

--- app/src/main/java/me/thanongsine/laounseen/fragments/RegisterFragment.java
+++ app/src/main/java/me/thanongsine/laounseen/fragments/RegisterFragment.java
@@ -1,27 +1,79 @@
 package me.thanongsine.laounseen.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 import me.thanongsine.laounseen.MainActivity;
 import me.thanongsine.laounseen.R;
 
 public class RegisterFragment extends Fragment {
-    Toolbar toolbar;
+    private Toolbar toolbar;
+    private Uri uri;
+    private ImageView imageView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_register, container, false);
 
-        toolbar = view.findViewById(R.id.toolbar);
+        initToolbar(view);
 
+        imageView = view.findViewById(R.id.imvPhoto);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Please Choose App"), 1);
+            }
+        });
+
+        return view;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        getActivity();
+        if (resultCode == Activity.RESULT_OK) {
+
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(Objects.requireNonNull(getActivity()).getContentResolver().openInputStream(uri));
+                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap, 400, 300, true);
+                imageView.setImageBitmap(bitmap1);
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            Toast.makeText(getActivity(), "Please Choose Photo", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void initToolbar(View view) {
+        toolbar = view.findViewById(R.id.toolbar);
         ((MainActivity)getActivity()).setSupportActionBar(toolbar);
         ((MainActivity)getActivity()).getSupportActionBar().setTitle("Register");
         ((MainActivity)getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
@@ -36,7 +88,23 @@ public class RegisterFragment extends Fragment {
                         .commit();
             }
         });
-
-        return view;
+        setHasOptionsMenu(true);
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_register, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.upload_item) {
+            //
+        }
+
+        return true;
+    }
+
 }
