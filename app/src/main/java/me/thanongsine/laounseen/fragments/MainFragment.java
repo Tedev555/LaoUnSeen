9bf3b4cfd0d4f0163ebf03454d515c2cd660fd86
ted555 <thanong984@gmail.com>
2018-08-07 14:03:48 +0700

    Add fragment_register.xml

--- app/src/main/java/me/thanongsine/laounseen/fragments/MainFragment.java
+++ app/src/main/java/me/thanongsine/laounseen/fragments/MainFragment.java
@@ -1,20 +1,45 @@
 package me.thanongsine.laounseen.fragments;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 import me.thanongsine.laounseen.R;
 
 public class MainFragment extends Fragment {
+    TextView registerTextView;
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+        registerTextView = view.findViewById(R.id.register_text_view);
+
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+
+                Objects.requireNonNull(getActivity()).getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.content_main, new RegisterFragment())
+                        .commit();
+            }
+        });
         return view;
     }
 }
