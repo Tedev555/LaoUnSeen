c9faaf1be2d85eaa5ed0852428029d37b993fcef
ted555 <thanong984@gmail.com>
2018-08-07 15:13:46 +0700

    Add logic to toolbar of register fragment and create transaction between MainFragment and Register Fragment

--- app/src/main/java/me/thanongsine/laounseen/fragments/RegisterFragment.java
+++ app/src/main/java/me/thanongsine/laounseen/fragments/RegisterFragment.java
@@ -4,17 +4,38 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import me.thanongsine.laounseen.MainActivity;
 import me.thanongsine.laounseen.R;
 
 public class RegisterFragment extends Fragment {
+    Toolbar toolbar;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_register, container, false);
+
+        toolbar = view.findViewById(R.id.toolbar);
+
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle("Register");
+        ((MainActivity)getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.content_main, new MainFragment())
+                        .commit();
+            }
+        });
 
         return view;
     }
